4d7408e9a615f783c8f3a37dc345f7472259ce1b
refute2change <madguyinthehouse@gmail.com>
2025-04-09 10:50:38 +0700

    optimizing entropy calculations

--- calculaterelevance.py
+++ calculaterelevance.py
@@ -1,52 +1,55 @@
 import loaddata
 from tqdm import tqdm
 import json
+from collections import Counter
+import time
 
 words = loaddata.load_data()
 
-def get_word_appearance(word: str, candidate: str) -> tuple[int]:
-    result = [0] * len(word)
-    letter_count = {}
-    unchecked = {}
+freqs = {}
+for word in words:
+    freqs[word] = Counter(word)
 
+def get_word_appearance(word: str, candidate: str) -> str:
+    result = ['m'] * len(word)
+    freq = freqs[candidate]
+    unchecked = []
     # First pass: Check for exact matches
     for i, (w, c) in enumerate(zip(word, candidate)):
         if w == c:
-            result[i] = 1
-            letter_count[w] = letter_count.get(w, 0) + 1
-        elif w in candidate:
-            unchecked[w] = unchecked.get(w, 0) + 1
+            result[i] = 'c'
+            freq[w] -= 1
+        elif freq.get(w, 0) == 0:
+            result[i] = 'w'
         else:
-            result[i] = -1
+            unchecked.append(i)
+    if len(unchecked) == 0:
+        return "".join(result)
 
     # Second pass: Check for misplaced matches
-    for i, w in enumerate(word):
-        if result[i] == 0:
-            if unchecked.get(w, 0) > 0:
-                criteria = letter_count.get(w, 0)
-                if criteria < candidate.count(w):
-                    unchecked[w] -= 1
-                    letter_count[w] = letter_count.get(w, 0) + 1
-                else:
-                    result[i] = -1
-            else:
-                result[i] = -1
+    for i in unchecked:
+        w = word[i]
+        result[i] = 'w' if freq.get(w, 0) <= 0 else 'm'
+        freq[w] -= 1
 
-    return tuple(result)
+    return "".join(result)
 
-def get_word_appearance_count(word: str, candidates: list[str]) -> dict[tuple[int], int]:
+def get_word_appearance_count(word: str, candidates: list[str]) -> dict[str, int]:
     appearance_count = {}
     for candidate in candidates:
         result = get_word_appearance(word, candidate)
         appearance_count[result] = appearance_count.get(result, 0) + 1
     return appearance_count
 
-json_obj = {}
-output_file = open("output.json", "w")
+t = time.process_time()
+print(get_word_appearance_count("apple", words))
+print(time.process_time() - t)
 
-for k in tqdm(range(len(words))):
-    i = words[k]
-    json_obj[i] = get_word_appearance_count(i, words)
+# json_obj = {}
+# output_file = open("output.json", "w")
 
-json.dump(json_obj, output_file, indent=4)
-output_file.close()
+# for k in tqdm(range(len(words))):
+#     i = words[k]
+#     json_obj[i] = get_word_appearance_count(i, words)
+
+# output_file.close()
